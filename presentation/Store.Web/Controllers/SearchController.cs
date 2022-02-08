71276f87ee7d0c2653a4363a479f70f42c9ff1fa
Danya Savytskiy <savitskiy.danilo.everything@gmail.com>
2022-02-08 16:51:02 +0200

    Добавить страницу поиска

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository BookRepository;
+
+        public SearchController(IBookRepository bookRepository)
+        {
+            BookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var books = BookRepository.GetAllByTitle(query);
+            return View(books);
+        }
+    }
+}
