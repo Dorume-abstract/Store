6e579dec9bcc554b3f3690c281986d67c61e60a9
Danya Savytskiy <savitskiy.danilo.everything@gmail.com>
2022-02-08 13:57:21 +0200

    Описать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        //public Book(string title)
+        //{
+        //    Title = title;
+        //}
+    }
+}
