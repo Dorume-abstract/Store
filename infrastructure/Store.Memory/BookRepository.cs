b57284682c9d1f2279774e3493d1fbcc2cdd6e6c
Dorume <savitskiy.danilo.everything@gmail.com>
2022-02-12 21:50:09 +0200

    Добавить страницу описание книги

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -7,13 +7,14 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "Art of Programming", "ISBN 123-123-123 0", "D. Knuth"),
-            new Book(2, "Refactoring", "ISBN 1234567890", "M. FLower"),
-            new Book(3, "C Programming Language", "ISBN 1597536548520", "B. Kernigan, D. Ritchie")
+            new Book(1, "The Art of Computer Programming", "ISBN 0321751043", "D. Knuth", "Descripton The Art of Computer Programming", 7.19m),
+            new Book(2, "Refactoring", "ISBN 1234567890", "M. FLower", "Description Refactoring", 12.45m),
+            new Book(3, "C Programming Language", "ISBN 1597536548520", "B. Kernigan, D. Ritchie", "Description C Programming Language", 14.98m)
         };
 
         public Book[] GetAllByTitleOrAuthor(string titleOrAuthor)
         {
+            if (titleOrAuthor == null) return new Book[0];
             return books.Where(book => book.Title.Contains(titleOrAuthor) 
                                        || book.Author.Contains(titleOrAuthor))
                 .ToArray();
@@ -21,8 +22,14 @@ namespace Store.Memory
 
         public Book[] GetAllByIsbn(string isbn)
         {
+            if (isbn == null) return new Book[0];
             return books.Where(book => book.Isbn.Contains(isbn))
                 .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
         }
     }
 }
